6f02e8b7cb580d62c9ad114dc4905cf4b56d28d9
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2025-03-01 10:27:41 +0100

    Django basic 1 - done

--- Project/musicApp/views.py
+++ Project/musicApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def all(request):
+  return HttpResponse("<h1>Tu będą wyświetlane informacje o albumach z dany zdanych</h1>")
+
+def details(request):
+  return HttpResponse("<h1>Tu będą wyświetlane informacje na temat wybranego albumu</h1>")
