56a1066a5c608b2f84f3bb129905a51a55763e12
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2025-03-01 10:36:04 +0100

    Create new Album model

--- Project/musicApp/models.py
+++ Project/musicApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Album(modes.Model):
+  title = models.CharField(max_length=64, blank=False, unique=False)
+  author = models.CharField(max_length=64, blank=False, unique=False)
+  genre = models.CharField(max_length=64, blank=False, unique=False)
+  release_date = models.DateField(null=False, blank=False)
+  # To be added later when new model Track will be aded
+  # tracks = models.
+  rating = models.DecimalField(max_digits=1, decimal_places=1, null=True, blank=True)
