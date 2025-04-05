e0ec412e3ffd2be8292df062ab13f41ccb4dafa5
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2025-04-05 22:47:21 +0200

    Project cleanup to start final one

--- Project/musicApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from musicApp.models import Album, AlbumDetails, Track, Genre
-
-admin.site.register(Album)
-admin.site.register(AlbumDetails)
-admin.site.register(Track)
-admin.site.register(Genre)
--- Project/musicApp/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from musicApp.models import Album
-
-class AlbumForm(ModelForm):
-  class Meta:
-    model = Album
-    fields = ['title', 'author']
