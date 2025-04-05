3076a89edbd82912531058d0bee67e9bbd384b2f
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2025-04-05 23:19:29 +0200

    Create basic models and first migration

--- bumbox/models.py
+++ bumbox/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  nickname = models.CharField(max_length=50, blank=False)
+  bio = models.TextField(blank=True)
+
+class Artist(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField(blank=True)
+  origin_country = models.CharField(max_length=50, blank=True)
+
+class Album(models.Model):
+  title = models.CharField(max_length=200)
+  artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='albums')
+
+class Track(models.Model):
+  title = models.CharField(max_length=200)
+  album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='tracks')
+  duration = models.DurationField()
+  genre = models.CharField(max_length=50, choices=[
+    ('POP', 'Pop'),
+    ('ROCK', 'Rock'),
+    ('RAP', 'Rap'),
+    ('OTHER', 'Other')
+  ])
+
+class Playlist(models.Model):
+  name = models.CharField(max_length=100)
+  profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='playlists')
+  tracks = models.ManyToManyField(Track, related_name='playlists')
+  created_at = models.DateTimeField(auto_now_add=True)
