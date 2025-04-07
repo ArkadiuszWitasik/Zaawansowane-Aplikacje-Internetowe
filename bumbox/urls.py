964e888fcf287cb53c55d990ed91b490a71c8f8c
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2025-04-07 19:37:11 +0200

    Add possibility to show artist details and delete it

--- bumbox/urls.py
+++ bumbox/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('profile', views.profile, name='profile'),
     path('artists', views.artists, name='artists'),
     path('artists/<int:pk>/', views.show_artist, name="artist-details"),
+    path('add_artist', views.add_artist, name='add-artist'),
+    path('delete_artist/<int:pk>/', views.delete_artist, name='delete-artist'),
     path('albums', views.albums, name='albums'),
     path('playlists', views.playlists, name='playlists'),
-    path('add_artist', views.add_artist, name='add-artist'),
 ]
--- bumbox/views.py
+++ bumbox/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
 from .forms import ArtistForm
 from .models import Artist
@@ -17,12 +17,6 @@ def show_artist(request, pk):
   artist = get_object_or_404(Artist, pk=pk)
   return render(request, 'public/artist_details.html', {'artist': artist})
 
-def albums(request):
-  return render(request, 'albums.html')
-
-def playlists(request):
-  return render(request, 'playlists.html')
-
 def add_artist(request):
   subbmited = False
 
@@ -37,3 +31,16 @@ def add_artist(request):
       subbmited = True
 
   return render(request, 'moderator/add_artist.html', {'form': form, 'subbmited': subbmited})
+
+def delete_artist(request, pk):
+  artist = get_object_or_404(Artist, pk=pk)
+  if request.method == "POST":
+    artist.delete()
+    return HttpResponseRedirect('/artists')
+  return render(request, 'moderator/delete_artist.html', {'artist': artist})
+
+def albums(request):
+  return render(request, 'albums.html')
+
+def playlists(request):
+  return render(request, 'playlists.html')
